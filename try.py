479cf6ebca58104d75f0ae5e09696bfa22fd88d5
NamrataTiwari <40715586+NamrataTiwari@users.noreply.github.com>
2022-03-07 21:57:38 +0530

    added div and mul

--- try.py
+++ try.py
@@ -2,3 +2,5 @@ print('welcome to git')
 a,b=10,20
 print(f'sum of two numbers {a+b}')
 print(f'difference of two numbers {a-b}')
+print(f'div of two numbers {a/b}')
+print(f'mul of two numbers {a*b}')
