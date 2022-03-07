5bbe3c1d380d1628bcffe72f3f42db210c2d3264
NamrataTiwari <40715586+NamrataTiwari@users.noreply.github.com>
2022-03-07 22:04:41 +0530

    added str

--- try.py
+++ try.py
@@ -4,3 +4,7 @@ print(f'sum of two numbers {a+b}')
 print(f'difference of two numbers {a-b}')
 print(f'div of two numbers {a/b}')
 print(f'mul of two numbers {a*b}')
+
+print('this is the main branch devoid of square and cube')
+
+print('adding str in main branch')
